51bd1e01bcf15af7cc4b7723def30dc23904a29e
Batool Almarzouq <53487593+BatoolMM@users.noreply.github.com>
2021-11-10 00:32:56 +0300

    New translations clean.py (Chinese Simplified)

--- book/website/Chinese Simplified/clean.py
+++ book/website/Chinese Simplified/clean.py
@@ -1,13 +1,13 @@
-"""A helper script to "clean up" all of your generated markdown and HTML files."""
-import shutil as sh
-from pathlib import Path
+"""一个帮助脚本来"清理"你生成的所有Markdown 和 HTML 文件。"""
+导入Shutil
+从 pathlib 导入路径
 
-path_root = Path(__file__).parent.parent
+path_root = 路径(__file__).parent.parent.parent
 
-paths = [path_root.joinpath('_site'),
+路径= [path_root.joinpath('_site'),
          path_root.joinpath('_build')]
-for path in paths:
-    print(f'Removing {path}...')
-    sh.rmtree(path, ignore_errors=True)
+路径的路径：
+    打印(f'正在删除 {path}...')
+    h.rmtree(路径，忽略错误=True)
 
-print('Done!')
+打印('完成!')

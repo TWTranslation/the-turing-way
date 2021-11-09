2321ad09a135a96a56d55bd32aae6794c2e5bef3
Batool Almarzouq <53487593+BatoolMM@users.noreply.github.com>
2021-11-10 00:33:18 +0300

    New translations contributors.sh (Spanish, Latin America)

--- book/website/Spanish, Latin America/contributors.sh
+++ book/website/Spanish, Latin America/contributors.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
-# Copy the latest contributors.md file content to afterword
-# this will publish the contributors file online in The Turing Way book
+# Copia el último contenido del archivo contributors.md a afterword
+# esto publicará el archivo de colaboradores online en el libro The Turing Way
 
-all_contributors='../../README.md' # Path to the main README file of The Turing Way
+all_contributors='../../README.md' # Ruta al archivo principal README de The Turing Way
 contributor_highlights='../../contributors.md'
 collaborating_projects='../../collaborating_projects.md'
 contributors_record='afterword/contributors-record.md'
 
-# Copy everything from the contributors highlight
-echo '(contributors-record-highlights)=' > $contributors_record
-cat $contributor_highlights >> $contributors_record
+# Copiar todo desde el resaltado de los colaboradores
+echo '(colaboradores-registro-resaltados)=' > $contributors_record
+gato $contributor_highlights >> $contributors_record
 
-# # Get linenumber where all contributors list starts
-echo '\n(contributors-record-all)=' >> $contributors_record
-echo '\n# All Contributors\n' >> $contributors_record
-echo '\n✨Using [all-contributors](https://allcontributors.org/) specification, _The Turing Way_ Recognises all contributors, not just the ones who push code. ✨' >> $contributors_record
-line_num=$(grep -n '## Contributors' $all_contributors | cut -d: -f1)
-echo '\n(contributors-record-contributors)=' >> $contributors_record
+# # Obtener línea donde comienza la lista de colaboradores
+echo '\n(colaboradores-record-all)=' >> $contributors_record
+echo '\n# Todos los colaboradores\n' >> $contributors_record
+echo '\n✨Usando la especificación [all-contributors](https://allcontributors.org/), _The Turing Way_ reconoce a todos los colaboradores, no solo a los que empujan el código. ✨' >> $contributors_record
+line_num=$(grep -n '## Colaboradores' $all_contributors | cortar -d: -f1)
+echo '\n(colaboradores-registro-contribuidores)=' >> $contributors_record
 
-tail -n +"$line_num" "$all_contributors" | while read line;
-do
+tail -n +"$line_num" "$all_contributors" | mientras se leía la línea;
+hacer
   echo $line >> $contributors_record
-done
+hecho
 
-# Copy everything from the collaborating projects files
-echo '\n(contributors-record-collaborators)=' >> $contributors_record
-cat $collaborating_projects >> $contributors_record
+# Copia todo desde los archivos de proyectos colaboradores
+echo '\n(colaboradores-registro-colaboradores)=' >> $contributors_record
+gato $collaborating_projects >> $contributors_record

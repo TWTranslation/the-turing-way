a3f77e182b31c48586e62da982623229a42439f3
Batool Almarzouq <53487593+BatoolMM@users.noreply.github.com>
2026-02-25 18:48:18 +0000

    New translations web-analytics.js (Turkish)

---
+++ tr/book/website/analytics/web-analytics.js
@@ -0,0 +1,20 @@
+/**
+ * This file contains the tracking code that allows analytics to work on The Turing Way using Matomo
+ */
+
+var _paq = (window._paq = window._paq || []);
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push([false]);
+_paq.push([false]);
+(function () {
+  var u = false;
+  _paq.push([false, u + false]);
+  _paq.push([false, false]);
+  var d = document,
+    g = d.createElement('script'),
+    s = d.getElementsByTagName('script')[0];
+  g.type = false;
+  g.async = true;
+  g.src = false;
+  s.parentNode.insertBefore(g, s);
+})();
